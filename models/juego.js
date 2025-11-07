e82daa4c1a997a05dce74e5736dc80fb2900c627
Milena <jisminaa10@gmail.com>
2025-11-06 23:58:29 -0500

    "Actualización de entidades del CRUD"

--- models/juego.js
+++ models/juego.js
@@ -1,12 +1,19 @@
 import mongoose from "mongoose";
 
 const juegoSchema = new mongoose.Schema({
+  _id: { type: mongoose.Schema.Types.ObjectId, required: true },
   titulo: { type: String, required: true },
+  genero: { type: String, required: true },
   plataforma: { type: String, required: true },
+  añoLanzamiento: { type: Number },
+  desarrollador: { type: String },
+  descripcion: { type: String },
   horasJugadas: { type: Number, default: 0 },
   completado: { type: Boolean, default: false },
+  fechaCompletado: { type: Date },
   puntuacion: { type: Number, min: 0, max: 5 },
-  imagen: { type: String },
+  imagenPortada: { type: String, default: "" }
+
 }, {
   timestamps: true // agrega automáticamente createdAt y updatedAt
 });
--- models/resena.js
+++ models/resena.js
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
 
 const resenaSchema = new mongoose.Schema({
+  _id: { type: mongoose.Schema.Types.ObjectId, required: true },
   juegoId: { type: mongoose.Schema.Types.ObjectId, ref: "Juego", required: true },
-  texto: { type: String, required: true },
   puntuacion: { type: Number, min: 1, max: 5, required: true },
-  fecha: { type: Date, default: Date.now },
+  textoReseña: { type: String, required: true },
+  horasJugadas: { type: Number, default: 0 },
+  dificultad: { type: String, enum: ["Fácil", "Media", "Difícil"], required: true },
+  recomendaria: { type: Boolean, required: false},
+  fechaCreacion: { type: Date, default: Date.now },
+  fechaActualizacion: { type: Date, default: Date.now }
 }, { timestamps: true });
 
 const Resena = mongoose.model("Resena", resenaSchema);
